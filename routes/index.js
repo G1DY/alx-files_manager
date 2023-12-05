28571defc82364891bec41af5236ce963b7f2ecf
Gideon <kipkerring.gedion@gmail.com>
2023-12-05 17:40:22 +0300

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable quotes */
+/* eslint-disable import/extensions */
 import { Express } from "express";
 import AppController from "../controllers/AppController";
-import { APIError, errorResponse } from "../middlewares/error";
+// import { APIError, errorResponse } from "../middlewares/error";
 
 const injectRoutes = (api) => {
   api.get("/status", AppController.getStatus);
