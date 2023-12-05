6e08404b32546ff55292254038bcd679a127aab1
Gideon <kipkerring.gedion@gmail.com>
2023-12-05 13:10:56 +0300

    AppController.js

---
+++ controllers/AppController.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable quotes */
+import redisClient from "../utils/redis";
+import dbClient from "../utils/db";
+
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()]).then(
+      ([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+        // eslint-disable-next-line comma-dangle
+      }
+    );
+  }
+}
