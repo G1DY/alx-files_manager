2c484652172363672ad37a946c81662ea70444dd
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2023-12-06 12:58:02 +0000

    Update routes file

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable quotes */
 /* eslint-disable import/extensions */
-import { Express } from "express";
+
 import AppController from "../controllers/AppController";
 import UsersController from "../controllers/UsersController";
+import AuthController from "../controllers/AuthController";
+import FilesController from "../controllers/FilesController";
 // import { APIError, errorResponse } from "../middlewares/error";
 
 const injectRoutes = (api) => {
@@ -10,6 +12,25 @@ const injectRoutes = (api) => {
   api.get("/stats", AppController.getStats);
 
   api.post("/users", UsersController.postNew);
+  api.get('/connect', AuthController.getConnect);
+
+  api.get('/disconnect', AuthController.getDisconnect);
+
+  api.get('/users/me', UsersController.getMe);
+
+  api.post('/users', UsersController.postNew);
+
+  api.post('/files', FilesController.postUpload);
+
+  api.get('/files/:id', FilesController.getShow);
+
+  api.get('/files', FilesController.getIndex);
+
+  api.put('/files/:id/publish', FilesController.putPublish);
+
+  api.put('/files/:id/publish', FilesController.putUnpublish);
+
+  api.get('/files:id/data', FilesController.getFile);
 };
 
 export default injectRoutes;
