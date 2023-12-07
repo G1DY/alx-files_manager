d173fcf0bd059ad2781c9c371bb228d3d493aa2d
Gideon <kipkerring.gedion@gmail.com>
2023-12-07 09:36:58 +0300

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,45 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable quotes */
 /* eslint-disable import/extensions */
-
-import AppController from "../controllers/AppController";
-import UsersController from "../controllers/UsersController";
-import AuthController from "../controllers/AuthController";
-import FilesController from "../controllers/FilesController";
-// import { APIError, errorResponse } from "../middlewares/error";
+// eslint-disable-next-line no-unused-vars
+import { Express } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+import { basicAutheticate, xTokenAutheticate } from '../middlewares/auth';
+import { APIError, errorResponse } from '../middlewares/error';
 
 const injectRoutes = (api) => {
-  api.get("/status", AppController.getStatus);
-  api.get("/stats", AppController.getStats);
-
-  api.post("/users", UsersController.postNew);
-  api.get('/connect', AuthController.getConnect);
-
-  api.get('/disconnect', AuthController.getDisconnect);
-
-  api.get('/users/me', UsersController.getMe);
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats);
 
   api.post('/users', UsersController.postNew);
+  api.get('/users/me', xTokenAutheticate, UsersController.getMe);
 
-  api.post('/files', FilesController.postUpload);
+  api.get('/connect', basicAutheticate, AuthController.getConnect);
+  api.get('/disconnect', AuthController.getDisconnect);
 
-  api.get('/files/:id', FilesController.getShow);
-
-  api.get('/files', FilesController.getIndex);
-
-  api.put('/files/:id/publish', FilesController.putPublish);
-
-  api.put('/files/:id/publish', FilesController.putUnpublish);
-
+  api.post('/files', xTokenAutheticate, FilesController.postUpload);
+  api.get('/files/:id', xTokenAutheticate, FilesController.getShow);
+  api.get('/files', xTokenAutheticate, FilesController.getIndex);
+  api.put('/files/:id/publish', xTokenAutheticate, FilesController.putPublish);
+  api.put(
+    '/files/:id/unpublish',
+    xTokenAutheticate,
+    FilesController.putUnpublish
+  );
   api.get('/files:id/data', FilesController.getFile);
+
+  api.all('*', (req, res, next) => {
+    errorResponse(
+      new APIError(404, `Cannot ${req.method} ${req.url}`),
+      req,
+      res,
+      next
+    );
+  });
+  api.use(errorResponse);
 };
 
 export default injectRoutes;
